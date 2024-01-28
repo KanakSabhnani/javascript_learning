8205b652119768a61a457c837a858e824ae747f0
KanakSabhnani <157870478+KanakSabhnani@users.noreply.github.com>
2024-01-28 15:25:21 +0000

    conversion

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,62 @@
+let score = 33
+console.log(typeof score) ;
+console.log(typeof(score)) ;
+
+
+let sc = "22"
+let valueInNumber = Number(sc)
+console.log(typeof(valueInNumber))
+// it will now give a number datatype 
+
+//but in this case :
+let sce = "33abcd"
+let valueSce = Number(sce)
+console.log(valueSce)
+//it will print not a number (nan)
+//in the output
+
+
+let sca = "null"
+let valueSca = Number(sca)
+//convert it to number:
+console.log(valueSca)
+//it will print 0
+
+
+let scb = "undefined"
+let valueScb = Number(scb)
+console.log(valueScb)
+//will print nan
+
+
+let scc = "true"
+let valueScc = Number(scc)
+console.log(valueScc)
+//it will print 1 :
+
+//"33" -> number
+//"33abc" -> NaN
+//true -> 1 ; false -> 0
+
+//converting to boolean :
+let isLoggedIn = "kanak"
+let boooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(boooleanIsLoggedIn);
+//it will print true
+
+let isLoggedIn1 = ""
+let boooleanIsLoggedIn1 = Boolean(isLoggedIn1)
+console.log(boooleanIsLoggedIn1);
+//it will print false 
+
+//1->true  0->false ""->false "kanak"->true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber)
+//it will print 33 only
+console.log(typeof someNumber)
+//the number has been converted to string now so it will print
+//string:
+
