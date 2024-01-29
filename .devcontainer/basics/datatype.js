b0640384ba85e3e45ea5fb6d3b05c34b02178ae1
KanakSabhnani <157870478+KanakSabhnani@users.noreply.github.com>
2024-01-29 14:17:20 +0530

    Update datatype.js

--- .devcontainer/basics/datatype.js
+++ .devcontainer/basics/datatype.js
@@ -60,3 +60,91 @@ console.log(typeof someNumber)
 //the number has been converted to string now so it will print
 //string:
 
+//*Operations/
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+console.log(+true)
+//will print 1
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
+
+//here == , === works differently 
+//than >= and <= 
+//no problem in basic conversion
+//like both operands are of same type
+
+//if the datatypes of both the 
+//operands are different then stucks 
+
+//in case of null sometimes it gets converted to 
+//nan (as done by equal equal to) and sometime to a number 0(as done by comparisons)
+
+
+
+//*********** datatypes*/
+//data is categorised into primitive and nonprimitive 
+//on the basis of how they are stored and how they are accessed
+
+//primitive (call by value)
+//7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+//Reference type[non-primitive]
+//Array , Objects , Functions
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+//it will return false because the return type of both will be different
+
+
+//reference type exmaples:
+const heroes = ["shaktiman" , "naagraj" , "doga"]
+let myObj = {
+    name: "kanak",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+//type of null is object
+//type of undefined is undefined
+//type of Boolean is boolean
+//type of Number is number 
+//type of String is string
+//type of Symbol is symbol
+//type of BigInt is bigint
+//type of Arrays is object
+//type of Function is function
+//type of Object is object
+
+
+//Stack(Primitive) , Heap(Non-primitive)
+
+let myYoutubeName = "kanak_sabhnani"
+let anotherName = myYoutubeName
+
+console.log(myYoutubeName)
+console.log(anotherName)
+//since a copy is passed only hence myYoutubeName is itself not changed
+
+let user = {
+    email: "user@google.com" ,
+    upi: "upi@google"
+}
+
+let user2 = user ;
+user2.email = "kanaksabhnani2002@gmail.com"
+user2.upi = "kanakupi@22"
+
+console.log(user.email)
+console.log(user.upi)
+//since they are passed with reference they get changed
+
